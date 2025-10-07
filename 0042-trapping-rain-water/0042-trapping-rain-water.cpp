7b5d89434b4ca9f4f7b4323f540aa408039e6381
RUDRA RAJ KIRAN <rajkiranraj0000@gmail.com>
2025-10-08 02:05:51 +0530

    Time: 1 ms (25.74%), Space: 27.4 MB (19.83%) - LeetHub

--- 0042-trapping-rain-water/0042-trapping-rain-water.cpp
+++ 0042-trapping-rain-water/0042-trapping-rain-water.cpp
@@ -1,21 +1,29 @@
 class Solution {
 public:
     int trap(vector<int>& height) {
-        int n = height.size();//size of the vector
-        vector <int> lmax(n,0);//vector for storing lmax value
-        vector <int> rmax(n,0);//vector for storing rmax value
-        lmax[0] = height[0];//intialise the first element as 0 least height is 0
-        rmax[n-1] = height[n-1];//intialise the last element as same as line 7 filing from right side so last element should be 0
-        for(int i = 1;i<n;i++){
-            lmax[i] = max(lmax[i-1],height[i]);
+        int n = height.size(); // Size of the vector
+        vector<int> lmax(n, 0); // Vector for storing the maximum height to the left of each bar
+        vector<int> rmax(n, 0); // Vector for storing the maximum height to the right of each bar
+        lmax[0] = height[0]; // Initialize the first element; the maximum height to the left is the bar itself
+        rmax[n - 1] = height[n - 1]; // Initialize the last element; the maximum height to the right is the bar itself
+        
+        // Calculate the maximum height to the left for each bar
+        for (int i = 1; i < n; i++) {
+            lmax[i] = max(lmax[i - 1], height[i]);
         }
-        for(int i = n-2;i>=0;i--){
-            rmax[i] = max(rmax[i+1],height[i]);
+        
+        // Calculate the maximum height to the right for each bar
+        for (int i = n - 2; i >= 0; i--) {
+            rmax[i] = max(rmax[i + 1], height[i]);
         }
-        int waterTrapped = 0;
-        for(int i = 0;i<n;i++){
-            waterTrapped += min(lmax[i],rmax[i])-height[i];
+        
+        int waterTrapped = 0; //  calculating the total amount of water trapped
+        
+        // Calculate the water trapped above each bar
+        for (int i = 0; i < n; ++i) {
+            waterTrapped += min(lmax[i], rmax[i]) - height[i];
         }
+        
         return waterTrapped;
     }
 };
