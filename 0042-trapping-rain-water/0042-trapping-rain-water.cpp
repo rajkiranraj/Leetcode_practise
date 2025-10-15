1bb6f9573feb78c396174dfef2c6c38eb97b7aa2
RUDRA RAJ KIRAN <rajkiranraj0000@gmail.com>
2025-10-15 10:23:48 +0530

    Time: 0 ms (100%), Space: 26.1 MB (60.9%) - LeetHub

--- 0042-trapping-rain-water/0042-trapping-rain-water.cpp
+++ 0042-trapping-rain-water/0042-trapping-rain-water.cpp
@@ -1,29 +1,25 @@
 class Solution {
 public:
     int trap(vector<int>& height) {
-        int n = height.size(); // Size of the vector
-        vector<int> lmax(n, 0); // Vector for storing the maximum height to the left of each bar
-        vector<int> rmax(n, 0); // Vector for storing the maximum height to the right of each bar
-        lmax[0] = height[0]; // Initialize the first element; the maximum height to the left is the bar itself
-        rmax[n - 1] = height[n - 1]; // Initialize the last element; the maximum height to the right is the bar itself
-        
-        // Calculate the maximum height to the left for each bar
-        for (int i = 1; i < n; i++) {
-            lmax[i] = max(lmax[i - 1], height[i]);
+        int n = height.size();//size of the array 
+        int left = 1;//left ptr 
+        int right = n - 1;//right ptr 
+        int leftMax = height[0];//left max
+        int rightMax = height[n - 1];//right max
+        int totalWater = 0;//total water 
+//two pointer approach 
+        while (left <= right) {
+            //consider using the left ptr if left<=right for correct lmax 
+            if (leftMax <= rightMax) {
+                totalWater += max(0, leftMax - height[left]);//cal total water from left max and the current ith index
+                leftMax = max(leftMax, height[left]);//update max every step 
+                left++;//move towards left 
+            } else {
+                totalWater += max(0, rightMax - height[right]);
+                rightMax = max(rightMax, height[right]);
+                right--;
+            }
         }
-        
-        // Calculate the maximum height to the right for each bar
-        for (int i = n - 2; i >= 0; i--) {
-            rmax[i] = max(rmax[i + 1], height[i]);
-        }
-        
-        int waterTrapped = 0; //  calculating the total amount of water trapped
-        
-        // Calculate the water trapped above each bar
-        for (int i = 0; i < n; ++i) {
-            waterTrapped += max(0,min(lmax[i], rmax[i]) - height[i]);
-        }
-        
-        return waterTrapped;
+        return totalWater;
     }
 };
