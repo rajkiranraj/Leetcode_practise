f809b0cc8f15d84bfa05c0b71db31f94a659da4c
RUDRA RAJ KIRAN <rajkiranraj0000@gmail.com>
2025-10-23 00:04:30 +0530

    Time: 4 ms (13.52%), Space: 17.7 MB (86.38%) - LeetHub

--- 0152-maximum-product-subarray/0152-maximum-product-subarray.cpp
+++ 0152-maximum-product-subarray/0152-maximum-product-subarray.cpp
@@ -5,11 +5,11 @@ public:
         int max_product = INT_MIN;
         int prefix,suffix; suffix = prefix = 1;
         for(int i = 0;i<nums.size();i++){
+            if(prefix == 0) prefix = 1;
+            if(suffix == 0) suffix = 1;
             prefix *= nums[i];
             suffix *= nums[nums.size()-i-1];
             max_product = max(max_product,max(prefix,suffix));
-            if(prefix == 0) prefix = 1;
-            if(suffix == 0) suffix = 1;
         }
         return max_product;
     }
