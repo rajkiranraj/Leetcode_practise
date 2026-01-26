32f20ab94b6fb1852e7573b1ebfd58261949e41c
RUDRA RAJ KIRAN <rajkiranraj0000@gmail.com>
2026-01-26 23:48:59 +0530

    Time: 0 ms (100%), Space: 9.8 MB (47.98%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.cpp
+++ 0242-valid-anagram/0242-valid-anagram.cpp
@@ -1,9 +1,25 @@
 class Solution {
 public:
     bool isAnagram(string s, string t) {
+        // if(s.size()!=t.size()) return false;
+        // sort(s.begin(),s.end());
+        // sort(t.begin(),t.end());
+        // return s==t;
         if(s.size()!=t.size()) return false;
-        sort(s.begin(),s.end());
-        sort(t.begin(),t.end());
-        return s==t;
+        vector <int> sfreq_arr(26,0);
+        // vector <int> tfreq_arr(26,0);
+        
+        for(char c: s){
+            sfreq_arr[c-'a']++;
+        }
+        for(char c: t){
+            sfreq_arr[c-'a']--;
+        }
+        for(int i: sfreq_arr){
+            if(i!=0){
+                return false;
+            }
+        }
+        return true;
     }
 };
