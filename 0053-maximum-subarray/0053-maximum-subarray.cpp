4a22f0a775ac8e1007c0d049f075467e7286b635
RUDRA RAJ KIRAN <rajkiranraj0000@gmail.com>
2026-01-28 21:58:59 +0530

    Time: 0 ms (100%), Space: 71.9 MB (15.7%) - LeetHub

--- 0053-maximum-subarray/0053-maximum-subarray.cpp
+++ 0053-maximum-subarray/0053-maximum-subarray.cpp
@@ -1,14 +1,15 @@
 class Solution {
 public:
-    int maxSubArray(vector<int>& nums) {
-        int size = nums.size();//size of the vector nums
-        int maxSum = INT_MIN;//smalllest possible int for all test cases
-        int crntSum = 0;//insitialising a var for storing the sum
-        for(int i = 0;i<size;i++){
-            crntSum += nums[i];//kadane's algorith with O(n)
-            maxSum = max(crntSum,maxSum);
-            if(crntSum < 0) crntSum = 0;
+    int maxSubArray(vector<int>& arr) {
+        int n = arr.size();
+        int sum = 0;int msum = INT_MIN;
+        for(int i = 0;i<n;i++){
+            sum = sum + arr[i];
+            msum = max(sum,msum);
+            if(sum<0){
+                sum=0;
+            }
         }
-        return maxSum;
+        return msum;
     }
 };
