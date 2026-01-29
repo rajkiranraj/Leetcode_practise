8a7923a69164adb4ee2cd0686e8d3128c71aaa58
RUDRA RAJ KIRAN <rajkiranraj0000@gmail.com>
2026-01-29 22:48:25 +0530

    Time: 0 ms (100%), Space: 8.7 MB (28.99%) - LeetHub

--- 0844-backspace-string-compare/0844-backspace-string-compare.cpp
+++ 0844-backspace-string-compare/0844-backspace-string-compare.cpp
@@ -1,47 +1,22 @@
 class Solution {
 public:
-    void isString(string s,vector<char>& s1){
-        for(char c: s){
-            if(isalnum(c)){
-                s1.push_back(c);
+    void buildString(string s,stack <char> &st){
+        for(int i = 0;i<s.size();i++){
+            if(s[i]!='#'){
+                st.push(s[i]);
             }
-            if(c=='#' && !(s1.empty())){
-                s1.pop_back();
+            else{
+                if(!st.empty()){
+                    st.pop();
+                }
             }
         }
     }
     bool backspaceCompare(string s, string t) {
-        vector <char> s1,s2;
-        isString(s,s1);
-        isString(t,s2);  
-        return s1==s2;
+        stack <char> st;
+        stack <char> st1;
+        buildString(s,st);
+        buildString(t,st1);
+        return st==st1;
     }
 };
-
-// class Solution {
-// public:
-//     bool backspaceCompare(string s, string t) {
-//         int i = s.length()-1;
-//         int j = t.length()-1;
-//         int sc = 0;
-//         int tc = 0;
-//         while(i!=0 && j!=0){
-//             while(i>0){
-//                 if(s[i]=='#'){
-//                     i--;
-//                     sc++;
-//                 }
-//                 else if(sc>0){
-//                     sc--;
-//                     i--;
-//                 }
-//                 else{
-//                     break;
-//                 }
-//             }
-//             while(j>0){
-                
-//             }
-//         }
-//     }
-// };
