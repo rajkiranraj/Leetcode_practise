bff7a95b7379e70df64125c7d79d1a3c173eb52d
RUDRA RAJ KIRAN <rajkiranraj0000@gmail.com>
2026-01-29 20:33:14 +0530

    Time: 0 ms (100%), Space: 10.3 MB (75.13%) - LeetHub

--- 0345-reverse-vowels-of-a-string/0345-reverse-vowels-of-a-string.cpp
+++ 0345-reverse-vowels-of-a-string/0345-reverse-vowels-of-a-string.cpp
@@ -1,23 +1,40 @@
-class Solution {
-public:
-    bool isVowel(char c) {
-        return c=='a'||c=='e'||c=='i'||c=='o'||c=='u'||c=='A'||c=='E'||c=='I'||c=='O'||c=='U';
-    }
-    string reverseVowels(string s1) {
-    //   transform(s1.begin(), s1.end(), s1.begin(), ::tolower);
-    int high = s1.length()-1;
-    int low = 0;
-    while(low<high){
-        if(!isVowel(s1[low])){
-           low++;
+class Solution
+{
+    public:
+        bool isVowel(char c)
+        {
+            return c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u' || c == 'A' || c == 'E' || c == 'I' || c == 'O' || c == 'U';
         }
-        else if(!isVowel(s1[high])){
-            high--;
+    string reverseVowels(string s)
+    {
+       	//Brute force
+       	//     vector<char> v;
+       	//     for(char c:s){
+       	//         if(c=='a'||c=='e'||c=='i'||c=='o'||c=='u'||c=='A'||c=='E'||c=='I'||c=='O'||c=='U'){
+       	//             v.push_back(c);
+       	//         }
+       	//     }
+       	// int j = v.size()-1;
+       	//    for(int i = 0; i < s.size(); i++){
+       	//         if(s[i]=='a'||s[i]=='e'||s[i]=='i'||s[i]=='o'||s[i]=='u'||
+       	//            s[i]=='A'||s[i]=='E'||s[i]=='I'||s[i]=='O'||s[i]=='U'){
+       	//             s[i] = v[j--];
+       	//         }
+       	//     }
+       	//    return s;
+
+       	//Optimal approach
+        int low = 0;
+        int high = s.size() - 1;
+        while (low < high)
+        {
+            if (isVowel(s[low]) && isVowel(s[high]))
+            {
+                swap(s[low++], s[high--]);
+            }
+            else if(!isVowel(s[low])) low++;
+            else high--;
         }
-        else{
-            swap(s1[low++],s1[high--]);
-        }
-    }
-      return s1;
+        return s;
     }
 };
