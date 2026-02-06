da423494eda55bb57a403f011c62e7a25f344adf
RUDRA RAJ KIRAN <rajkiranraj0000@gmail.com>
2026-02-06 22:39:37 +0530

    Time: 0 ms (100%), Space: 23.9 MB (19.32%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.cpp
+++ 0283-move-zeroes/0283-move-zeroes.cpp
@@ -4,7 +4,8 @@ public:
         int j = 0;
         for(int i = 0;i<arr.size();i++){
            if(arr[i]!=0){
-            swap(arr[j++],arr[i]);
+            swap(arr[j],arr[i]);
+            j++;
            }
         }
     }
