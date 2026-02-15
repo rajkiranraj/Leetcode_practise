5795d1787a359741163e1c332b5ccb29cade565b
RUDRA RAJ KIRAN <rajkiranraj0000@gmail.com>
2026-02-15 20:08:28 +0530

    Time: 0 ms (100%), Space: 28.4 MB (7.83%) - LeetHub

---
+++ 0169-majority-element/0169-majority-element.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int majorityElement(vector<int>& nums) {
+        unordered_map<int,int> m;
+        int mjCheck = nums.size()/2;
+        for(int i = 0;i<nums.size();i++){
+            m[nums[i]]++;
+        }
+        for(auto it:m){
+            if(it.second>mjCheck){
+                return it.first;
+            }
+        }
+        return 0;
+    }
+};
