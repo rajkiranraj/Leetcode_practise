5690359256e1c39bf8154eb0330d324b0923119e
RUDRA RAJ KIRAN <rajkiranraj0000@gmail.com>
2026-02-15 20:15:46 +0530

    Time: 0 ms (100%), Space: 11.5 MB (97.02%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.cpp
+++ 0075-sort-colors/0075-sort-colors.cpp
@@ -1,6 +1,20 @@
 class Solution {
 public:
-    void sortColors(vector<int>& nums) {
-        sort(nums.begin(),nums.end());
+    void sortColors(vector<int>& arr) {
+        //Brute force
+        // sort(nums.begin(),nums.end());
+
+        int low,mid,high;
+        low = mid = 0;
+         high = arr.size()-1;
+        while(mid<=high){
+            if(arr[mid] == 1) mid++;
+            else if(arr[mid] == 0){
+                swap(arr[mid++],arr[low++]);
+            }
+            else{
+                swap(arr[high--],arr[mid]);
+            }
+        }
     }
 };
