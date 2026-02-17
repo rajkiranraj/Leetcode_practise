bb8f0b83f1154f4a54100e376f3c7093bf50751d
RUDRA RAJ KIRAN <rajkiranraj0000@gmail.com>
2026-02-17 20:45:37 +0530

    Time: 0 ms (100%), Space: 28 MB (98.76%) - LeetHub

--- 0169-majority-element/0169-majority-element.cpp
+++ 0169-majority-element/0169-majority-element.cpp
@@ -1,16 +1,44 @@
+
+// MOORE'S VOTING ALGORITHM
 class Solution {
 public:
     int majorityElement(vector<int>& nums) {
-        unordered_map<int,int> m;
-        int mjCheck = nums.size()/2;
-        for(int i = 0;i<nums.size();i++){
-            m[nums[i]]++;
+        int n = nums.size();
+        int element = -1;
+        int count = 0;
+        for (int i = 0; i < n; i++) {
+            if (count == 0) {
+                count = 1;
+                element = nums[i];
+            } else if (element == nums[i])
+                count++;
+            else
+                count--;
+            // //if the element always exists then no need to use this loop
+            // int cnt = 0;
+            // for(int i :nums){
+            //     if(i==element) cnt++;
+            // }
+            // if(cnt > n/2) return el;
         }
-        for(auto it:m){
-            if(it.second>mjCheck){
-                return it.first;
-            }
-        }
-        return 0;
+        return element;
     }
 };
+
+// BRUTE - FORCE - ALGORITH
+//  class Solution {
+//  public:
+//      int majorityElement(vector<int>& nums) {
+//          unordered_map<int,int> m;
+//          int mjCheck = nums.size()/2;
+//          for(int i = 0;i<nums.size();i++){
+//              m[nums[i]]++;
+//          }
+//          for(auto it:m){
+//              if(it.second>mjCheck){
+//                  return it.first;
+//              }
+//          }
+//          return 0;
+//      }
+//  };
