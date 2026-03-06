d2f9a0e8fac006eed8248644d34dcea80150d87e
RUDRA RAJ KIRAN <rajkiranraj0000@gmail.com>
2026-03-06 12:53:48 +0530

    Time: 0 ms (100%), Space: 8.1 MB (61.9%) - LeetHub

--- 0796-rotate-string/0796-rotate-string.cpp
+++ 0796-rotate-string/0796-rotate-string.cpp
@@ -1,20 +1,22 @@
-// class Solution {
-// public:
-//     bool rotateString(string s, string goal) {
-        
-//     }
-// };
-
-
 class Solution {
 public:
     bool rotateString(string s, string goal) {
         int n = s.size();
         if(s.size()!=goal.size()) return false;
-        for(int i = 0;i<n;i++){
-            rotate(s.begin(),s.begin()+1,s.end());
-            if(s==goal) return true;
-        }
-        return false;
+        return (s+s).find(goal)!=string::npos;
     }
 };
+
+
+// class Solution {
+// public:
+//     bool rotateString(string s, string goal) {
+//         int n = s.size();
+//         if(s.size()!=goal.size()) return false;
+//         for(int i = 0;i<n;i++){
+//             rotate(s.begin(),s.begin()+1,s.end());
+//             if(s==goal) return true;
+//         }
+//         return false;
+//     }
+// };
