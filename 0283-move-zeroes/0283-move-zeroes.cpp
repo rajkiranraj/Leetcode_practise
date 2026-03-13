89c8387dfb9b67aee4c9df04b9469f6bcac82687
RUDRA RAJ KIRAN <rajkiranraj0000@gmail.com>
2026-03-13 19:44:39 +0530

    Time: 0 ms (100%), Space: 23.8 MB (81.74%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.cpp
+++ 0283-move-zeroes/0283-move-zeroes.cpp
@@ -1,12 +1,13 @@
 class Solution {
 public:
     void moveZeroes(vector<int>& nums) {
-        int i = 0;int n = nums.size();
-        for(int j = 0;j < n; j++){
-            if(nums[j]!=0){
-                swap(nums[i],nums[j]);
-                i++;
-            }
+       int n = nums.size();
+       int i = 0;
+       for(int j = 0;j < n; j++){
+        if(nums[j]!=0){
+            swap(nums[j],nums[i]);
+            i++;
         }
+       }
     }
 };
