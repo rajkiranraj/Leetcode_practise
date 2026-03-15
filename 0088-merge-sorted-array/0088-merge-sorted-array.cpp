f54d8db1287648e08c03a2986125053a5971aab6
RUDRA RAJ KIRAN <rajkiranraj0000@gmail.com>
2026-03-15 23:09:39 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0088-merge-sorted-array/0088-merge-sorted-array.cpp
+++ 0088-merge-sorted-array/0088-merge-sorted-array.cpp
@@ -1,23 +1,46 @@
 class Solution {
 public:
     void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
-        vector <int> res;
-        int i = 0, j = 0;
-        while(i < m && j < n){
-            if(nums1[i]<= nums2[j]){
-                res.push_back(nums1[i++]);
+        int lidx = m+n-1;
+        int i = m-1;
+        int j = n-1;
+        while(i >= 0 && j>=0){
+            if(nums1[i]>=nums2[j]){
+                nums1[lidx--] = nums1[i--];
             }else{
-                res.push_back(nums2[j++]);
+                nums1[lidx--] = nums2[j--];
             }
         }
-        while(i < m){
-                res.push_back(nums1[i++]);
-        }while(j < n){
-                res.push_back(nums2[j++]);
+        while(i>=0){
+            nums1[lidx--] = nums1[i--];
         }
-        int idx = 0;
-        for(int i:res){
-            nums1[idx++] = i;
+        while(j>=0){
+            nums1[lidx--] = nums2[j--];
         }
     }
 };
+
+
+// class Solution {
+// public:
+//     void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
+//         vector <int> res;
+//         int i = 0, j = 0;
+//         while(i < m && j < n){
+//             if(nums1[i]<= nums2[j]){
+//                 res.push_back(nums1[i++]);
+//             }else{
+//                 res.push_back(nums2[j++]);
+//             }
+//         }
+//         while(i < m){
+//                 res.push_back(nums1[i++]);
+//         }while(j < n){
+//                 res.push_back(nums2[j++]);
+//         }
+//         int idx = 0;
+//         for(int i:res){
+//             nums1[idx++] = i;
+//         }
+//     }
+// };
