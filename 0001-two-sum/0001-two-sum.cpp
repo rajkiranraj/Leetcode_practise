c1576238ae107259ecdc4dc7f27a6beb93f6c4f8
RUDRA RAJ KIRAN <rajkiranraj0000@gmail.com>
2026-03-28 17:31:47 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0001-two-sum/0001-two-sum.cpp
+++ 0001-two-sum/0001-two-sum.cpp
@@ -2,16 +2,14 @@ class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
         int n = nums.size();
-        vector<int>v;
         unordered_map <int,int> m;
         for(int i = 0;i < n; i++){
             int to_find = target-nums[i];
             if(m.find(to_find)!=m.end()){
-                v.push_back(i);
-                v.push_back(m[to_find]);
+                return{i,m[to_find]};
             }
             m[nums[i]] = i;
         }
-        return v;
+        return {};
     }
 };
