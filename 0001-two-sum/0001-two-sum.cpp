93fb533045c26ac8fa7670abc22fbb96b2d2c09b
RUDRA RAJ KIRAN <rajkiranraj0000@gmail.com>
2026-03-28 17:31:15 +0530

    Time: 7 ms (42.13%), Space: 16 MB (5.14%) - LeetHub

--- 0001-two-sum/0001-two-sum.cpp
+++ 0001-two-sum/0001-two-sum.cpp
@@ -2,54 +2,16 @@ class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
         int n = nums.size();
+        vector<int>v;
         unordered_map <int,int> m;
-        for(int i = 0;i<n;i++){
-            int firstValue = nums[i];
-            int secondValue = target - firstValue;
-            if(m.find(secondValue) != m.end()){
-                return {i,m[secondValue]};
+        for(int i = 0;i < n; i++){
+            int to_find = target-nums[i];
+            if(m.find(to_find)!=m.end()){
+                v.push_back(i);
+                v.push_back(m[to_find]);
             }
-            m[firstValue]=i;
+            m[nums[i]] = i;
         }
-        return {};
+        return v;
     }
 };
-
-// class Solution {
-// public:
-//     vector<int> twoSum(vector<int>& nums, int target) {
-//         int n = nums.size();
-//         vector <pair<int,int>> v;
-//         for(int i = 0; i < n ; i++){
-//             v.push_back({nums[i],i});
-//         }
-//         int i = 0;int j = n-1;
-//         sort(v.begin(),v.end());
-//         while(i<j){
-//             int sum = v[i].first + v[j].first;
-//             if(sum == target) return {v[i].second,v[j].second};
-//             else if(sum > target) j--;
-//             else i++;
-//         }
-//         return {};
-//     }
-// };
-
-
-// class Solution {
-// public:
-//     vector<int> twoSum(vector<int>& nums, int target) {
-//       int i = 0;int j = nums.size()-1;
-//       sort(nums.begin(),nums.end());
-//       while(i<j){
-//         int sum = nums[i]+nums[j];
-//         if(sum>target){
-
-//         }
-//         else{
-
-//         }
-//       }
-//       return {i,j};
-//     }
-// };
