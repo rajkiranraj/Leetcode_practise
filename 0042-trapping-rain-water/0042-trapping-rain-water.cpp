17ef6c9f0fdaabe17265970a23b123ac0173d192
RUDRA RAJ KIRAN <rajkiranraj0000@gmail.com>
2026-04-02 22:36:07 +0530

    Time: 0 ms (100%), Space: 27.4 MB (6.55%) - LeetHub

--- 0042-trapping-rain-water/0042-trapping-rain-water.cpp
+++ 0042-trapping-rain-water/0042-trapping-rain-water.cpp
@@ -1,17 +1,29 @@
 class Solution {
 public:
-    int trap(vector<int>& height) {int n = height.size();
-       vector <int> left(n);vector <int> right(n);
-       left[0]=height[0];right[n-1]=height[n-1];
-       for(int i = 1;i < n; i++){
-        left[i] = max(left[i-1],height[i]);
-       }for(int i = n-2;i >= 0; i--){
-        right[i] = max(right[i+1],height[i]);
-       } 
-       int total = 0;
-       for(int i = 0;i < n; i++){
-        total += min(left[i],right[i]) - height[i];
-       }
-       return total;
+    int trap(vector<int>& height) {
+        int n = height.size(),total = 0;
+        vector<int> leftmax(n),rightmax(n);
+        leftmax[0] = height[0];rightmax[n-1] = height[n-1];
+        for(int i = 1;i<n;i++)
+        leftmax[i] = max(leftmax[i-1],height[i]);
+        for(int i = n-2;i>=0;i--)
+        rightmax[i] = max(rightmax[i+1],height[i]);
+        for(int i = 0;i < n; i++)
+        total += min(leftmax[i],rightmax[i]) - height[i];
+        return total;
     }
 };
+
+
+// class Solution {
+// public:
+//     int trap(vector<int>& height) {int n = height.size();
+//         int total = 0;
+//         for(int i = 0;i < n; i++){int leftmax = 0,rightmax = 0;
+//             for(int j = 0;j <= i; j++) leftmax = max(leftmax,height[j]);
+//             for(int j = i;j < n; j++)rightmax = max(rightmax,height[j]);
+//             total += min(leftmax,rightmax) - height[i];
+//         }
+//         return total;
+//     }
+// };
