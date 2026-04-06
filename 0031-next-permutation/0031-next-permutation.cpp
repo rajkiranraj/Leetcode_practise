a9d30e86dde6f5f157f3910e0b2a10b271534af8
RUDRA RAJ KIRAN <rajkiranraj0000@gmail.com>
2026-04-06 16:09:30 +0530

    Time: 0 ms (100%), Space: 15.6 MB (92.65%) - LeetHub

--- 0031-next-permutation/0031-next-permutation.cpp
+++ 0031-next-permutation/0031-next-permutation.cpp
@@ -1,25 +1,30 @@
 class Solution {
 public:
     void nextPermutation(vector<int>& nums) {
-        int pivot = -1;int n = nums.size();
-        for(int i = n-2;i>=0;i--){
+        //pivot element 
+        int n = nums.size();
+        int pivot = -1;
+        for(int i = n-2;i >= 0; i--){
             if(nums[i]<nums[i+1]){
                 pivot = i;
                 break;
             }
         }
-        if(pivot==-1){
+        if(pivot == -1){
             reverse(nums.begin(),nums.end());
             return ;
         }
         int jstGreater = -1;
-        for(int i = n-1;i>pivot;i--){
+        for(int i = n-1;i > pivot; i--){
             if(nums[i]>nums[pivot]){
                 jstGreater = i;
+                swap(nums[jstGreater], nums[pivot]); 
+                reverse(nums.begin()+pivot+1,nums.end());
                 break;
             }
         }
-        swap(nums[pivot],nums[jstGreater]);
-        reverse(nums.begin()+pivot+1,nums.end());
     }
 };
+
+
+ 
