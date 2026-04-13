f49d1eff506bfd978861d31e1f79a2f3bdad9c37
RUDRA RAJ KIRAN <rajkiranraj0000@gmail.com>
2026-04-13 23:52:31 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0042-trapping-rain-water/0042-trapping-rain-water.cpp
+++ 0042-trapping-rain-water/0042-trapping-rain-water.cpp
@@ -1,48 +1,23 @@
 class Solution {
 public:
     int trap(vector<int>& height) {
-        int n = height.size();
-        int i = 0, j = height.size()-1;
-        int lmax = 0,rmax = 0,total = 0;
-        while(i < j){
-            lmax = max(lmax,height[i]);
-            rmax = max(rmax,height[j]);
-            if(lmax <= rmax){
-                total += lmax - height[i++];
-            }else{
-                total += rmax - height[j--];
-            }
+      int n = height.size();
+      int left = 0;
+      int right = n-1;
+      int leftMax = height[0];
+      int rightMax = height[n-1];int total = 0;
+      while(left<=right){
+        if(height[left]<=height[right]){
+            leftMax = max(leftMax,height[left]);
+            total += leftMax - height[left];
+            left++;
         }
-        return total;
+        else{
+            rightMax = max(rightMax,height[right]);
+            total += rightMax - height[right];
+            right--;
+        }
+      }
+      return total;
     }
 };
-
-// class Solution {
-// public:
-//     int trap(vector<int>& height) {
-//         int n = height.size(),total = 0;
-//         vector<int> leftmax(n),rightmax(n);
-//         leftmax[0] = height[0];rightmax[n-1] = height[n-1];
-//         for(int i = 1;i<n;i++)
-//         leftmax[i] = max(leftmax[i-1],height[i]);
-//         for(int i = n-2;i>=0;i--)
-//         rightmax[i] = max(rightmax[i+1],height[i]);
-//         for(int i = 0;i < n; i++)
-//         total += min(leftmax[i],rightmax[i]) - height[i];
-//         return total;
-//     }
-// };
-
-
-// class Solution {
-// public:
-//     int trap(vector<int>& height) {int n = height.size();
-//         int total = 0;
-//         for(int i = 0;i < n; i++){int leftmax = 0,rightmax = 0;
-//             for(int j = 0;j <= i; j++) leftmax = max(leftmax,height[j]);
-//             for(int j = i;j < n; j++)rightmax = max(rightmax,height[j]);
-//             total += min(leftmax,rightmax) - height[i];
-//         }
-//         return total;
-//     }
-// };
