6d74a2bb2b9edaa2b6cd181a243ba7ec3bc4de99
RUDRA RAJ KIRAN <rajkiranraj0000@gmail.com>
2026-04-25 00:55:17 +0530

    Time: 9 ms (7.84%), Space: 8.7 MB (37.52%) - LeetHub

---
+++ 0009-palindrome-number/0009-palindrome-number.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    bool isPalindrome(int x) {
+        long long dup = x;
+         if(x<0){
+            dup = dup * -1;
+        }
+        long long rev = 0;
+       
+        while(x!=0){
+            long long digit = x%10;
+            rev=rev*10+digit;
+            x/=10;
+        }
+        if(dup==rev)return 1;
+        return 0;
+    }
+};
